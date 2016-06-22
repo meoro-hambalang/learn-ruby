4be0762852d3fd9f9da8d930dfa57c65a8fdcc1a
Dung Nguyen <dung.nguyen@employmenthero.com>
2016-06-22 18:02:22 +0700

    add `return` scenario for learning block, also add some explaination

--- learning_block.rb
+++ learning_block.rb
@@ -7,12 +7,17 @@ def my_method()
     yield('custom_param')
   end
 
+  puts '....done'
+
 end
 
 
 puts '1>'
 puts my_method
-# output: Inside my_method()
+# output:
+#Inside my_method()
+#....done
+
 
 
 puts
@@ -24,23 +29,42 @@ end)
 #Inside my_method()
 #There is a block
 #Inside block: custom_param
+#....done
+
 
 
 puts
 puts '3>'
 puts my_method do |param|
   puts "Inside block: #{param}"
-end
+end # this `do..end` block belongs to `puts` instead of `my_method`, the same as `puts(my_method) do |param| ... end`
 #output:
 #Inside my_method()
+#....done
+
 
 
 puts
 puts '4>'
 puts my_method { |param|
   puts "Inside block: #{param}"
+} # this `{}` block belongs to `my_method`, the same as `puts(my_method {|param| ... })`
+#output:
+#Inside my_method()
+#There is a block
+#Inside block: custom_param
+#....done
+
+
+
+puts
+puts '5>'
+puts my_method { |param|
+  puts "Inside block: #{param}"
+  return 'Return in block' #this will raise error since block cannot return
 }
 #output:
 #Inside my_method()
 #There is a block
 #Inside block: custom_param
+#unexpected return (LocalJumpError)
