310e5564a4eb1b49cb6c4dd0e2d8ac20063f57be
Dung Nguyen <dung.nguyen@employmenthero.com>
2016-06-22 17:45:56 +0700

    add more scenarios for learning block

--- learning_block.rb
+++ learning_block.rb
@@ -1,37 +1,46 @@
-class MyClass
-  def method_1(param)
-    puts "Inside block with param: #{param}"
-  end
-end
-
 def my_method()
   puts 'Inside my_method()'
 
-  MyClass.new.method_1('custom_param')
-
   #call block
   if block_given?
-    yield()
+    puts 'There is a block'
+    yield('custom_param')
   end
 
 end
 
-def method_2(param1, param2)
-
-end
 
 puts '1>'
-puts my_method # output: Inside my_method()
+puts my_method
+# output: Inside my_method()
 
 
 puts
 puts '2>'
-puts my_method() { |param|
-  puts "Inside block with param: #{param}"
-}
-
-
-
+puts(my_method do |param|
+  puts "Inside block: #{param}"
+end)
 #output:
 #Inside my_method()
-#Inside block
+#There is a block
+#Inside block: custom_param
+
+
+puts
+puts '3>'
+puts my_method do |param|
+  puts "Inside block: #{param}"
+end
+#output:
+#Inside my_method()
+
+
+puts
+puts '4>'
+puts my_method { |param|
+  puts "Inside block: #{param}"
+}
+#output:
+#Inside my_method()
+#There is a block
+#Inside block: custom_param
